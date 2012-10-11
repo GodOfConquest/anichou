1e1a18ef81149dfd68619eafa5b1651b0b916ab6
Sir Anthony <anthony@adsorbtion.org>
2012-10-11 22:08:21 +0600

    Progress bar.

---
+++ src/AniChou/gui/widgets/classes/ACProgressBarDelegate.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from PyQt4 import QtCore, QtGui
+from PyQt4.QtGui import QStyle, QApplication
+
+
+class ACProgressBarDelegate(QtGui.QItemDelegate):
+
+    def paint(self, painter, option, index):
+        # Set up a QStyleOptionProgressBar to precisely mimic the
+        # environment of a progress bar.
+        progressBarOption = QtGui.QStyleOptionProgressBarV2();
+        progressBarOption.state = QStyle.State_Enabled;
+        progressBarOption.direction = QApplication.layoutDirection();
+        progressBarOption.rect = option.rect;
+        progressBarOption.fontMetrics = QApplication.fontMetrics();
+        progressBarOption.minimum = 0;
+        progressBarOption.maximum = 100;
+        progressBarOption.textAlignment = QtCore.Qt.AlignCenter;
+        progressBarOption.textVisible = True;
+
+        # Set the progress and text values of the style option.
+        #progress = (parent())->clientForRow(index.row())->progress();
+        progress = index.data().toPyObject()
+        progressBarOption.progress = 0 if progress < 0 else progress
+        progressBarOption.text = QtCore.QString("{0}%".format(progressBarOption.progress));
+
+        # Draw the progress bar onto the view.
+        QApplication.style().drawControl(QStyle.CE_ProgressBar, progressBarOption, painter);
